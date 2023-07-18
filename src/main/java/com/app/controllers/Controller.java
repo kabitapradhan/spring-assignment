ec6bd2cb918ed1ab35b213b6fe31392041551540
Kabita <pradhankabita7873@gmail.com>
2023-07-18 12:22:32 +0530

    add crossorigin annotaion

--- src/main/java/com/app/controllers/Controller.java
+++ src/main/java/com/app/controllers/Controller.java
@@ -3,6 +3,7 @@ package com.app.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.app.service.UserService;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin
 public class Controller {
 	@Autowired
 	private UserService userService;
